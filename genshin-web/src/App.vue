125d1fb1e1351caa3f4206ff64aba6457a2f01e7
jinmu333 <dreamhunter2333@gmail.com>
2023-05-05 22:29:50 +0800

    feat: update web style

--- genshin-web/src/App.vue
+++ genshin-web/src/App.vue
@@ -1,25 +1,30 @@
 <script setup>
-import { NGrid, NGi, NInput, NButton, NSpace, NCard, NSpin, NInputNumber, NFormItem } from 'naive-ui'
-import { onMounted, ref } from "vue";
+import { NInput, NButton, NSpace, NCard, NSpin, NInputNumber, NFormItem } from 'naive-ui'
+import { ref } from "vue";
 import MarkdownIt from 'markdown-it';
 
 const API_BASE = import.meta.env.VITE_API_BASE || "";
 const md = new MarkdownIt();
+md.renderer.rules.image = function (tokens, idx, options, env, self) {
+  let imgToken = tokens[idx];
+  let src = imgToken.attrs[0][1];
+  return `<img src="${src}" style="max-width: 100%; height: auto;" />`;
+}
 
 const uid = ref(null);
 const command = ref(null);
 const result = ref("");
 const loading = ref(false);
 
-
-const onSubmit = async () => {
+const onSubmit = async (isUid) => {
   try {
     loading.value = true;
+    let curCommand = isUid ? `#绑定${uid.value}` : (command.value || "#帮助");
     const response = await fetch(`${API_BASE}/api/genshin`, {
       method: "POST",
       body: JSON.stringify({
         uid: uid.value,
-        command: command.value || "#帮助",
+        command: curCommand,
       }),
       headers: { "Content-Type": "application/json" },
     });
@@ -37,54 +42,36 @@ const onSubmit = async () => {
   }
 };
 
-
-onMounted(() => {
-  (window.adsbygoogle = window.adsbygoogle || []).push({});
-  (window.adsbygoogle = window.adsbygoogle || []).push({});
-});
 </script>
 
 <template>
-  <n-grid x-gap="12" :cols="6">
-    <n-gi>
-      <div class="side">
-        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8486711392183862" data-ad-slot="9515796661"
-          data-ad-format="auto" data-full-width-responsive="true"></ins>
-      </div>
-    </n-gi>
-    <n-gi span="4">
-      <n-spin :show="loading">
-        <div class="main">
-          <n-space vertical>
-            <h1>原</h1>
-            <h4>本项目仅供娱乐，请先发送 "#绑定uid"</h4>
-            <div style="display: inline-block;">
-              <n-form-item label="UID" label-placement="left">
-                <n-input-number v-model:value="uid" :min="100000000" :max="900000000" placeholder="请输入UID" />
-              </n-form-item>
-              <n-form-item label="指令" label-placement="left">
-                <n-input v-model:value="command" type="textarea" placeholder="#帮助" />
-              </n-form-item>
-            </div>
-            <div class="button-container">
-              <n-button class="center" @click="onSubmit" tertiary round type="primary">
-                发送
-              </n-button>
-            </div>
-            <n-card title="结果">
-              <div class="result" v-html="result"></div>
-            </n-card>
-          </n-space>
+  <n-spin :show="loading">
+    <div class="main">
+      <n-space vertical>
+        <h1>原</h1>
+        <h4>本项目仅供娱乐，请先点击绑定，#帮助 #喵喵帮助 查看指令</h4>
+        <div style="display: inline-block;">
+          <n-form-item label="UID" label-placement="left">
+            <n-input-number v-model:value="uid" :min="100000000" :max="900000000" placeholder="请输入UID" />
+          </n-form-item>
+          <n-form-item label="指令" label-placement="left">
+            <n-input v-model:value="command" type="textarea" placeholder="#帮助 #喵喵帮助" />
+          </n-form-item>
         </div>
-      </n-spin>
-    </n-gi>
-    <n-gi>
-      <div class="side">
-        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8486711392183862" data-ad-slot="3072024858"
-          data-ad-format="auto" data-full-width-responsive="true"></ins>
-      </div>
-    </n-gi>
-  </n-grid>
+        <div class="button-container">
+          <n-button class="center" @click="onSubmit(true)" tertiary round type="primary">
+            绑定
+          </n-button>
+          <n-button class="center" @click="onSubmit(false)" tertiary round type="primary">
+            发送
+          </n-button>
+        </div>
+        <n-card v-if="result">
+          <div class="result" v-html="result"></div>
+        </n-card>
+      </n-space>
+    </div>
+  </n-spin>
 </template>
 
 <style scoped>
@@ -121,6 +108,6 @@ onMounted(() => {
 }
 
 .result {
-  text-align: left;
+  text-align: center;
 }
 </style>
